60af3f513a005db91b218882c29cd45207bfb69e
XuanDatNguyen <77032222+XuanDatNguyen@users.noreply.github.com>
2022-08-30 17:24:29 +0700

    test nginx

--- uploader/views.py
+++ uploader/views.py
@@ -4,64 +4,13 @@ from django.conf import settings
 from .models import File
 import boto3
 import io
-# upload transfer from other source in binary mode, use below
-s3 = boto3.client('s3')
+# s3 = boto3.client('s3', )
+s3 = boto3.client(
+    's3',
+    aws_access_key_id = settings.AWS_S3_ACCESS_KEY_ID, 
+    aws_secret_access_key=settings.AWS_S3_SECRET_ACCESS_KEY,
+)
 # to upload from local storage, use below
-# s3 = boto3.resource('s3')
-
-# def upload(request):
-#     if request.method == 'POST':  
-#         print(request.POST)
-#         file = request.FILES['file'].read()
-#         fileName= request.POST['filename']
-#         print(fileName)
-#         existingPath = request.POST['existingPath']
-#         end = request.POST['end']
-#         nextSlice = request.POST['nextSlice']
-        
-#         if file=="" or fileName=="" or existingPath=="" or end=="" or nextSlice=="":
-#             res = JsonResponse({'data':'Invalid Request'})
-#             return res
-#         else:
-#             if existingPath == 'null':
-#                 path = 'fileUploader/media/' + fileName
-#                 # path = 'media/' + fileName
-#                 with open(path, 'wb+') as destination: 
-#                     destination.write(file)
-#                 FileFolder = File()
-#                 FileFolder.existingPath = fileName
-#                 FileFolder.eof = end
-#                 FileFolder.name = fileName
-#                 FileFolder.save()
-#                 if int(end):
-#                     res = JsonResponse({'data':'Uploaded Successfully','existingPath': fileName})
-#                 else:
-#                     res = JsonResponse({'existingPath': fileName})
-#                 return res
-
-#             else:
-#                 path = 'fileUploader/media/' + fileName
-#                 # path = 'media/' + existingPath
-#                 model_id = File.objects.get(existingPath=existingPath)
-#                 if model_id.name == fileName:
-#                     if not model_id.eof:
-#                         with open(path, 'ab+') as destination: 
-#                             destination.write(file)
-#                         if int(end):
-#                             model_id.eof = int(end)
-#                             model_id.save()
-#                             res = JsonResponse({'data':'Uploaded Successfully','existingPath':model_id.existingPath})
-#                         else:
-#                             res = JsonResponse({'existingPath':model_id.existingPath})    
-#                         return res
-#                     else:
-#                         res = JsonResponse({'data':'EOF found. Invalid request'})
-#                         return res
-#                 else:
-#                     res = JsonResponse({'data':'No such file exists in the existingPath'})
-#                     return res
-#     return render(request, 'upload.html')
-
 def upload(request):
     if request.method == 'POST':  
         print(request.POST)
