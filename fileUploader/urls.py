83e0c0f9cc5e8b2837f3ab9a7b4c58cbc57b19ec
mci247 <85936528+mci247@users.noreply.github.com>
2022-09-20 13:23:04 +0700

    update files

--- fileUploader/urls.py
+++ fileUploader/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('uploader.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
